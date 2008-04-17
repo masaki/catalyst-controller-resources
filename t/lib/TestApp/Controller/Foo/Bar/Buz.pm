695f1cd56b60d5f72ed1591fdbf9c17fe3b4075f
Daisuke Murase <typester@unknownplace.org>
2008-04-17 03:27:30 +0000

    @masaki:
    Controller::Foo::Bar::Buz のような下の階層のコントローラでも使いたいのだけど、こういう場合どうしたらいいんでしょう？
    とりあえずテストだけコミット。
    
    というのも、実際に使おうとしたときに既存のアプリケーションの /api/ 以下(Controller::API::*)で使いたいなぁと思ったのですが、うまく動いてくれなくて。
    
    なんか方法あったら教えてほしいです！
    
    
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Catalyst-Controller-Resources/trunk@9609 d0d07461-0603-4401-acd4-de1884942a52

---
+++ t/lib/TestApp/Controller/Foo/Bar/Buz.pm
@@ -0,0 +1,15 @@
+package TestApp::Controller::Foo::Bar::Buz;
+use strict;
+use warnings;
+use base 'Catalyst::Controller::Resources';
+
+sub list {}
+sub post {}
+sub create {}
+sub show {}
+sub update {}
+sub destroy {}
+sub edit {}
+
+1;
+
